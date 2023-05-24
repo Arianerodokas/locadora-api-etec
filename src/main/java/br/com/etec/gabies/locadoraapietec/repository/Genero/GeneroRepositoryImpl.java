932ecdfe6d78c2dc3e8a2a9ebfc3ed441e697a75
Arianerodokas <barbosaarianerodokas@gmail.com>
2023-05-24 08:56:14 -0300

    paginacao finalizada

--- src/main/java/br/com/etec/gabies/locadoraapietec/repository/Genero/GeneroRepositoryImpl.java
+++ src/main/java/br/com/etec/gabies/locadoraapietec/repository/Genero/GeneroRepositoryImpl.java
@@ -4,6 +4,7 @@ import br.com.etec.gabies.locadoraapietec.model.Genero;
 import br.com.etec.gabies.locadoraapietec.repository.filter.GeneroFilter;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
@@ -23,7 +24,7 @@ public class GeneroRepositoryImpl implements GeneroRepositoryQuery {
     private EntityManager manager;
 
     @Override
-    public Page<Genero> filtrar(GeneroFilter generoFilter, Pageable pageble) {
+    public Page<Genero> filtrar(GeneroFilter generoFilter, Pageable pageable) {
 
         CriteriaBuilder builder = manager.getCriteriaBuilder();
         CriteriaQuery<Genero> criteria = builder.createQuery(Genero.class);
@@ -34,9 +35,36 @@ public class GeneroRepositoryImpl implements GeneroRepositoryQuery {
         criteria.orderBy(builder.asc(root.get("descricao")));
 
         TypedQuery<Genero> query = manager.createQuery(criteria);
-
-        return null;
+        adicionarRestricoesDePaginacao(query, pageable);
+        
+        return new PageImpl<>(query.getResultList(), pageable, total(generoFilter));
     }
+
+    private Long total(GeneroFilter generoFilter) {
+
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Genero> root = criteria.from(Genero.class);
+
+        Predicate[] predicates = criarRestricoes(generoFilter, builder, root);
+        criteria.where(predicates);
+        criteria.orderBy(builder.asc(root.get("descricao")));
+
+        criteria.select(builder.count(root));
+        return manager.createQuery(criteria).getSingleResult();
+
+    }
+
+    private void adicionarRestricoesDePaginacao(TypedQuery<Genero> query, Pageable pageable) {
+        int paginaAtual = pageable.getPageNumber();
+        int totalRegistroPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaAtual * totalRegistroPagina;
+
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totalRegistroPagina);
+
+    }
+
 
     private Predicate[] criarRestricoes(GeneroFilter generoFilter, CriteriaBuilder builder, Root<Genero> root) {
         List<Predicate> predicates = new ArrayList<>();
--- src/main/java/br/com/etec/gabies/locadoraapietec/resource/GeneroResource.java
+++ src/main/java/br/com/etec/gabies/locadoraapietec/resource/GeneroResource.java
@@ -2,7 +2,10 @@ package br.com.etec.gabies.locadoraapietec.resource;
 
 import br.com.etec.gabies.locadoraapietec.model.Genero;
 import br.com.etec.gabies.locadoraapietec.repository.GeneroRepository;
+import br.com.etec.gabies.locadoraapietec.repository.filter.GeneroFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,10 @@ public class GeneroResource {
     @GetMapping("/todos")
     public List<Genero> listarTodosGeneros(){
         return generoRepository.findAll(Sort.by("descricao").ascending());
+    }
+
+    public Page<Genero> pesquisar(GeneroFilter generoFilter, Pageable pageable) {
+        return generoRepository.filtrar(generoFilter, pageable);
     }
 
 }
